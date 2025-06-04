6890de1fb2e4d08973d040388eeb58aff2be92c7
gitalsg <alsg24@student.bth.se>
2025-06-04 12:12:54 +0200

    Kmom10 - set up done

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProjectController extends AbstractController
+{
+    #[Route("/proj", name: "project")]
+    public function project(): Response
+    {
+        return $this->render('proj/project.html.twig');
+    }
+
+    #[Route("/proj/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('proj/about.html.twig');
+    }
+}
