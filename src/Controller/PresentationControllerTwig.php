d94355e88869377f962fca585199da4493088791
gitalsg <alsg24@student.bth.se>
2025-06-05 13:19:09 +0200

    Kmom10 - changed some routes in presentation controller

--- src/Controller/PresentationControllerTwig.php
+++ src/Controller/PresentationControllerTwig.php
@@ -32,43 +32,43 @@ class PresentationControllerTwig extends AbstractController
         return $this->render('metrics.html.twig');
     }
 
-    #[Route("/report#kmom01", name: "kmom01")]
+    #[Route("/report/kmom01", name: "kmom01")]
     public function kmom01(): Response
     {
         return $this->render('kmom01.html.twig');
     }
 
-    #[Route("/report#kmom02", name: "kmom02")]
+    #[Route("/report/kmom02", name: "kmom02")]
     public function kmom02(): Response
     {
         return $this->render('kmom02.html.twig');
     }
 
-    #[Route("/report#kmom03", name: "kmom03")]
+    #[Route("/report/kmom03", name: "kmom03")]
     public function kmom03(): Response
     {
         return $this->render('kmom03.html.twig');
     }
 
-    #[Route("/report#kmom04", name: "kmom04")]
+    #[Route("/report/kmom04", name: "kmom04")]
     public function kmom04(): Response
     {
         return $this->render('kmom04.html.twig');
     }
 
-    #[Route("/report#kmom05", name: "kmom05")]
+    #[Route("/report/kmom05", name: "kmom05")]
     public function kmom05(): Response
     {
         return $this->render('kmom05.html.twig');
     }
 
-    #[Route("/report#kmom06", name: "kmom06")]
+    #[Route("/report/kmom06", name: "kmom06")]
     public function kmom06(): Response
     {
         return $this->render('kmom06.html.twig');
     }
 
-    #[Route("/report#kmom10", name: "kmom10")]
+    #[Route("/report/kmom10", name: "kmom10")]
     public function kmom10(): Response
     {
         return $this->render('kmom10.html.twig');
